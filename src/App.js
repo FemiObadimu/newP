0ddb0c0085f00e475cc531d93315d99c85182841
Femi Obadimu <66454378+FemiObadimu@users.noreply.github.com>
2022-08-18 14:06:26 +0100

    ready

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
